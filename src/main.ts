ae84c652f33bb24bb309e7388c71a95c99dc9cc6
Abdul Rafay Modi <74718326+AlphaRomeoMike@users.noreply.github.com>
2024-10-13 16:57:56 +0500

    - Remove global handler

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ResponseInterceptor } from './response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['debug'] });
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
